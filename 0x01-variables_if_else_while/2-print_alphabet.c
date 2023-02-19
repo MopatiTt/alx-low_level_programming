88c143ac779b122834b245b52c66a1c8a88cb751
MopatiTt <hagitot@yahoo.com>
2023-02-19 01:47:16 -0800

    move files to right location

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints the lowercase alphabet followed by a new line
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int i;
+    int i;
 
-	for (i = 97; i <= 122; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
+    for (i = 97; i <= 122; i++)
+    {
+        putchar(i);
+    }
+    
+    putchar('\n');
 
-	return (0);
+    return 0;
 }
 
